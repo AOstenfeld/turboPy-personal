263c03dd97482df21557dc4e342309a51a1c80e1
AOstenfeld <66341877+AOstenfeld@users.noreply.github.com>
2020-06-18 12:33:07 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,7 +3,7 @@ a = 0
 b = 1
 print(a)
 print(b) # These lines take care of displaying the first 2 fibonacci numbers
-while b + a < n: # Continues to print fibonacci numbers until the next loop will produce a number > n
+while b + a <= n: # Continues to print fibonacci numbers until the next loop will produce a number > n
     temp = a # Creates a temporary variable that points to the same value as a
     a = b # a now points to the same value as b
     b = a + temp # This line is equivalent to b = b + a or b += a, produces the next fibonacci number
